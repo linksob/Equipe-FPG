ed2e1c30cec5fbe074354b53c49ad73c55d39f36
MatheusBarison <87202687+MatheusBarison@users.noreply.github.com>
2022-11-10 17:34:50 -0300

    Update sbProject.cpp

--- sbProject.cpp
+++ sbProject.cpp
@@ -15,12 +15,13 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 
+
+
 float year = 0, day = 0, revolution = 0, earthDist = 3.0, moonDist = 0.9;
-static int view = 15, x = 0;  
+static int view = 15, x = 0;
 //x é a variável que rege quem sofrerá a mudança de massa (x = 0 -> weight sun increase; x = 1 -> weight sun decrease; x = 2 -> weight earth increase; x = 3 -> weight earth decrease)
 
-
-void init(void) 
+void init(void)
 {
    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
    GLfloat mat_shininess[] = { 50.0 };
@@ -48,24 +49,38 @@ void reshape(int w,int h)
     glLoadIdentity();
 }
 
-void drawSystem()
-{
-   gluLookAt (0, view, 3, 0.0, 0.0, 0.0, 0, 1.0, 0); //posição da câmera
-   glColor3f(1.0, 1.0, 0.0);
-   glPushMatrix();
-   glTranslatef(0,0,-4);
-   glutSolidSphere(1.5, 20, 16);   /* draw sun */
-   glRotatef ((GLfloat) year, 0.0, 1.0, 0.0);
-   glTranslatef (earthDist, 0.0, 0.0);
-   glColor3f(0.0, 0.0, 1.0);
-   glRotatef ((GLfloat) day, 0.0, 1.0, 0.0);
-   glutWireSphere(0.6, 20, 16);    /* draw earth */
-   glTranslatef (moonDist, 0.0, 0.0);
-   glColor3f(1.0, 1.0, 1.0);
-   glutWireSphere(0.2, 20, 16);  /* draw moon */
-   //glRotatef ((GLfloat) revolution, 0.0, 1.0, 0.0);
-   glPopMatrix();
+void pointOfView(){
+    gluLookAt (0, view, 3, 0.0, 0.0, 0.0, 0, 1.0, 0); //posição da câmera
 
+}
+
+void drawSun(){
+    glColor3f(1.0, 1.0, 0.0);
+    glPushMatrix();
+    glTranslatef(0,0,-4);
+    glutSolidSphere(1.5, 20, 16);   /* draw sun */
+    //glPopMatrix();
+}
+
+void drawEarth(){
+    glPushMatrix();
+    glColor3f(0.0, 0.0, 1.0);
+    glRotatef ((GLfloat) day, 0.0, 1.0, 0.0);
+    glTranslatef (earthDist, 0.0, 0.0);
+    glutSolidSphere(0.6, 20, 16);    /* draw earth */
+    //glPopMatrix();
+}
+
+void drawMoon(){
+    glPushMatrix();
+    glTranslatef (moonDist, 0.0, 0.0);
+    glColor3f(1.0, 1.0, 1.0);
+    glutSolidSphere(0.2, 20, 16);  /* draw moon */
+    glRotatef ((GLfloat) revolution, 0.0, 1.0, 0.0);
+    glPopMatrix();
+}
+
+void incrementation(){
    //incremento na translação da terra (em torno do sol
    year = year + 0.01;
    if(year > 360) year = year - 360;
@@ -75,8 +90,8 @@ void drawSystem()
    if(day > 360) day = day - 360;
 
    //incremento na translação da lua (em torno da terra)
-   //revolution = revolution + 0.03;
-   //if(revolution > 360) revolution = revolution - 360;
+   revolution = revolution + 0.03;
+   if(revolution > 360) revolution = revolution - 360;
 }
 
 void display(void)
@@ -84,7 +99,11 @@ void display(void)
    	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa cada posição anterior
    	glClearColor (0.0,0.0,0.0,1.0);
    	glLoadIdentity();
-   	drawSystem();
+   	pointOfView();
+   	drawSun();
+   	drawEarth();
+   	drawMoon();
+   	incrementation();
    	glutSwapBuffers(); //swap the buffers
 }
 
@@ -94,7 +113,7 @@ void gravityChange(int x){
    switch (x) {
       case 0:
          earthDist = earthDist + 1;
-         break;          
+         break;
       case 1:
       	 earthDist = earthDist - 1;
       	 break;
@@ -102,8 +121,8 @@ void gravityChange(int x){
       	 moonDist = moonDist + 0.1;
          break;
       case 3:
-         moonDist = moonDist - 0.1;      
-         break;    
+         moonDist = moonDist - 0.1;
+         break;
       case 27:
          exit(0);
          break;
@@ -143,8 +162,8 @@ void keyboard (unsigned char key, int x, int y)
 }
 
 int main(int argc, char** argv)
-{	
-   glutInit(&argc, argv);   
+{
+   glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
    glutInitWindowSize (1000, 1000);
    glutInitWindowPosition (100, 100);
