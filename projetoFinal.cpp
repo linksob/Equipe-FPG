19c20a6d948eb5b9c2e9f6324ffc7d211a96cf53
linksob <107320569+linksob@users.noreply.github.com>
2022-12-05 21:56:12 -0300

    Adicionando texto

--- projetoFinal.cpp
+++ projetoFinal.cpp
@@ -1,21 +1,10 @@
-/*
- * Copyright (c) 1993-1997, Silicon Graphics, Inc.
- * ALL RIGHTS RESERVED
- * Permission to use, copy, modify, and distribute this software for
- * any purpose and without fee is hereby granted, provided that the above
- * copyright notice appear in all copies and that both the copyright notice
- * and this permission notice appear in supporting documentation, and that
- * the name of Silicon Graphics, Inc. not be used in advertising
- * or publicity pertaining to distribution of the software without specific,
- * written prior permission.
- */
 
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string>
 
 float year = 0, day = 0, revolution = 0, earthDist = 3.0, moonDist = 0.9;
 static int view = 15, x = 0, velocity = 1;
@@ -26,12 +15,10 @@ void init(void)
 {
    float lightposition[] = {0, 0, 0, 1};
    glLightfv(GL_LIGHT0, GL_POSITION, lightposition);
-
-
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
-
+   glEnable(GL_TEXTURE_2D);
 }
 
 void reshape(int w,int h)
@@ -48,6 +35,34 @@ void pointOfView(){
     gluLookAt (0, view, 3, 0.0, 0.0, 0.0, 0, 1.0, 0); //posição da câmera
 }
 
+ void drawText(const std::string& text, const float x, const float y, const float r, const float g, const float b,bool seek)
+  {
+    glDisable(GL_LIGHTING);
+    glDisable(GL_TEXTURE_2D);
+    glDisable(GL_DEPTH_TEST);
+    if (!seek) {
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+    }
+    glColor3f(r, g, b);
+    glRasterPos2f(x, y);
+    //int i, len;
+    //len = (int)strlen(text);
+    for (const char c : text)
+      glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, (int)c);
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_DEPTH_TEST);
+  }
+
 void drawSun(){
     //Propriedades do material
     GLfloat mat_colorSun[] = {1.0, 1.0, 0.0, 0.0};
@@ -56,6 +71,7 @@ void drawSun(){
     //Transformações geométricas
     glTranslatef(0,0,-4);
     glutSolidSphere(1.5, 20, 16);   /* draw sun */
+    drawText("                Sol",0,0.3,0,1,0,true);
 }
 
 void drawEarth(){
@@ -68,6 +84,7 @@ void drawEarth(){
     glTranslatef (earthDist, 0.0, 0.0);
     glRotatef ((GLfloat) day, 0.0, 1.0, 0.0);
     glutSolidSphere(0.6, 20, 16);    /* draw earth */
+    drawText("     Terra",0,0,0,1,0,true);
 }
 
 void drawMoon(){
@@ -78,6 +95,7 @@ void drawMoon(){
     //Transformações geométricas
     glTranslatef (moonDist, 0.0, 0.0);
     glutSolidSphere(0.2, 20, 16);  /* draw moon */
+    drawText("  Lua",0,0,0,1,0,true);
     glPopMatrix();
 }
 
@@ -107,11 +125,33 @@ void desenhoOrbita(GLdouble tamanho){
     glPopMatrix();
     gluDeleteQuadric(disk);
 }
+float dist = 149.6;
+float calculaForca(float dist)
+{
+        float m1 = 1.9, m2 = 5.9, G = 6.7;
+        float f;
+        f = (m1 * m2 * G)*1000 / (dist * dist);
+        return f;
+
+}
+
+//int font = GLUT_BITMAP_TIMES_ROMAN_24
 
 void display(void)
 {
    	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa cada posição anterior
    	glClearColor (0.0,0.0,0.0,1.0);
+   	drawText("Gravity Simulator",-0.125,0.85,1,1,1,false);
+   	drawText("Distance:" + std::to_string(dist) + " milions",0.5,-0.6,1,1,1,false);
+    float forca = calculaForca(dist);
+   	drawText("Force:" + std::to_string(forca) + " E22",0.5,-0.7,1,1,1,false);
+   	drawText("A: Aumentar raio da orbita (Terra)",-0.8,-0.3,1,1,1,false);
+   	drawText("S: Diminuir raio da orbita (Terra)",-0.8,-0.4,1,1,1,false);
+   	drawText("D: Aumentar raio da orbita (Lua)",-0.8,-0.5,1,1,1,false);
+   	drawText("F: Diminuir raio da orbita (Lua)",-0.8,-0.6,1,1,1,false);
+   	drawText("H: Aumentar velocidade da orbita (Terra)",-0.8,-0.7,1,1,1,false);
+   	drawText("L: Diminuir velocidade da orbita (Terra)",-0.8,-0.8,1,1,1,false);
+   	drawText("V: Mudar perspectiva",-0.8,-0.9,1,1,1,false);
    	glLoadIdentity();
    	pointOfView();
    	drawSun();
@@ -132,6 +172,7 @@ void gravityChange(int x){
       case 1:
          if(earthDist >= 3){
             earthDist = earthDist*(0.75);
+            dist = dist / 2 ;
          }
       	 break;
       case 2:
@@ -164,10 +205,13 @@ void keyboard (unsigned char key, int x, int y)
           break;
       case 'a':
       case 'A':
-      	  gravityChange(0);
+        dist = dist * 2 ;
+        calculaForca(dist);
+        gravityChange(0);
       	  break;
       case 's':
       case 'S':
+
       	  gravityChange(1);
       	  break;
       case 'd':
@@ -200,7 +244,7 @@ int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-   glutInitWindowSize (1000, 1000);
+   glutInitWindowSize (960, 640);
    glutInitWindowPosition (100, 100);
    glutCreateWindow (argv[0]);
    init();
